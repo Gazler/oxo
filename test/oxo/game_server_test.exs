6f370679675e27abd158656b0a291943ef21eb57
Gary Rennie <gazler@gmail.com>
2016-06-11 22:21:11 +0100

    defactor(GameServer): make game_won function horrible

--- test/oxo/game_server_test.exs
+++ test/oxo/game_server_test.exs
@@ -53,12 +53,8 @@ defmodule Oxo.GameServerTest do
   end
 
   test "play will continue until the X wins", context do
-    {:ok, pid: pid, state: _, player_1: player_1, player_2: player_2} = started_game(context)
-    assert {:ok, _} = GameServer.play(pid, 0, player_1)
-    assert {:ok, _} = GameServer.play(pid, 3, player_2)
-    assert {:ok, _} = GameServer.play(pid, 1, player_1)
-    assert {:ok, _} = GameServer.play(pid, 4, player_2)
-    assert {:ok, state} = GameServer.play(pid, 2, player_1)
+    {:ok, pid: pid, state: state, player_1: _, player_2: _} = started_game(context)
+    state = set_grid(pid, state, [0, 0, 0, 1, 1])
     assert state.winner == 0
     assert state.status == :finished
     assert state.win_line == [0, 1, 2]
@@ -68,13 +64,8 @@ defmodule Oxo.GameServerTest do
   end
 
   test "play will continue until the O wins", context do
-    {:ok, pid: pid, state: _, player_1: player_1, player_2: player_2} = started_game(context)
-    assert {:ok, _} = GameServer.play(pid, 0, player_1)
-    assert {:ok, _} = GameServer.play(pid, 3, player_2)
-    assert {:ok, _} = GameServer.play(pid, 1, player_1)
-    assert {:ok, _} = GameServer.play(pid, 4, player_2)
-    assert {:ok, _} = GameServer.play(pid, 6, player_1)
-    assert {:ok, state} = GameServer.play(pid, 5, player_2)
+    {:ok, pid: pid, state: state, player_1: _, player_2: _} = started_game(context)
+    state = set_grid(pid, state, [0, 0, nil, 1, 1, 1, 0])
     assert state.winner == 1
     assert state.status == :finished
     assert state.win_line == [3, 4, 5]
@@ -84,16 +75,8 @@ defmodule Oxo.GameServerTest do
   end
 
   test "play will continue until there is a draw", context do
-    {:ok, pid: pid, state: _, player_1: player_1, player_2: player_2} = started_game(context)
-    assert {:ok, _} = GameServer.play(pid, 0, player_1)
-    assert {:ok, _} = GameServer.play(pid, 3, player_2)
-    assert {:ok, _} = GameServer.play(pid, 1, player_1)
-    assert {:ok, _} = GameServer.play(pid, 4, player_2)
-    assert {:ok, _} = GameServer.play(pid, 6, player_1)
-    assert {:ok, _} = GameServer.play(pid, 7, player_2)
-    assert {:ok, _} = GameServer.play(pid, 8, player_1)
-    assert {:ok, _} = GameServer.play(pid, 2, player_2)
-    assert {:ok, state} = GameServer.play(pid, 5, player_1)
+    {:ok, pid: pid, state: state, player_1: _, player_2: _} = started_game(context)
+    state = set_grid(pid, state, [0, 0, 1, 1, 1, 0, 0, 1, 0])
     assert state.winner == false
     assert state.status == :finished
     assert state.win_line == []
@@ -102,11 +85,52 @@ defmodule Oxo.GameServerTest do
                            0, 1, 0]
   end
 
+  test "all win conditions" do
+    wins = [
+      [0, 0, 0, nil, nil, nil, nil, 1, 1],
+      [1, 1, nil, 0, 0, 0, nil, nil, nil],
+      [1, 1, nil, nil, nil, nil, 0, 0, 0],
+
+      [0, 1, 1, 0, nil, nil, 0, nil, nil],
+      [1, 0, 1, nil, 0, nil, nil, 0, nil],
+      [1, 1, 0, nil, nil, 0, nil, nil, 0],
+
+      [0, 1, 1, nil, 0, nil, nil, nil, 0],
+      [1, 1, 0, nil, 0, nil, 0, nil, nil],
+    ]
+
+    Enum.each(wins, fn win ->
+      {:ok, pid} = GameServer.start_link()
+      {:ok, pid: pid, state: state, player_1: _, player_2: _} = started_game(%{pid: pid})
+      state = set_grid(pid, state, win)
+      assert state.winner == 0
+    end)
+  end
+
   defp started_game(context) do
     pid = context.pid
     {:ok, _} = GameServer.join(pid, "123")
     {:ok, state} = GameServer.join(pid, "456")
     [player_1, player_2] = state.players
     {:ok, pid: pid, state: state, player_1: player_1, player_2: player_2}
+  end
+
+  defp set_grid(pid, state, moves) do
+    moves |> Enum.with_index() |> Enum.reduce(%{}, fn {move, index}, acc ->
+      Map.update(acc, move, [index], &(&1 ++ [index]))
+    end)
+    |> play(pid, 0, state)
+  end
+
+  def play(%{0 => [], 1 => []}, _pid, _current, state), do: state
+  def play(%{0 => [head | tail]} = moves, pid, 0, %{players: [player_1 | _]}) do
+    {:ok, state} = GameServer.play(pid, head, player_1)
+    moves = Map.put(moves, 0, tail)
+    play(moves, pid, 1, state)
+  end
+  def play(%{1 => [head | tail]} = moves, pid, 1, %{players: [_, player_2]}) do
+    {:ok, state} = GameServer.play(pid, head, player_2)
+    moves = Map.put(moves, 1, tail)
+    play(moves, pid, 0, state)
   end
 end
